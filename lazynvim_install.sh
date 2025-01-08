5d37eb740d44270882fe28f684075cdd8462af6b
YaniXIV <165547725+YaniXIV@users.noreply.github.com>
2025-01-07 21:53:17 -0700

    Update lazynvim_install.sh
    Added gcc to dependencies.

--- lazynvim_install.sh
+++ lazynvim_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+#needed dependencies for lazy vim
 sudo apt update
-sudo apt install -y git curl neovim
+sudo apt install -y git curl neovim gcc
 
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-
-
+#rm -rf ~/.config/nvim/plugins
+#optional, removes example directory, its good for inspiration but not necessary.
+ 
+#optional plugin install
 cat <<EOF >> ~/.config/nvim/lua/plugins.lua
 
 -- Custom plugin
